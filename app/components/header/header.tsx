7151b1642cb0b949b1c11129f1399eb5696b252e
ewerton reis <ewertonreisc01@gmail.com>
2024-03-12 18:42:14 -0300

    header improvement

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,24 +1,38 @@
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 
-export default function Header({ showNavigation }) {
+export default function Header() {
+  const pathname = usePathname()
+
   return (
     <header className="w-full flex justify-between items-center bg-neutral-500 h-12">
-      <div>
+      <div className="ml-5">
         <Link href="/">
-          <h1 className="text-white text-lg font-bold">Meu Site</h1>
+          <h1 className="text-yellow-300  hover:text-yellow-400 text-lg font-bold">
+            Fut433
+          </h1>
         </Link>
       </div>
       <nav>
-        <ul className="w-auto mr-10 flex items-center justify-between">
-          {showNavigation && (
+        <ul className="w-auto h-12 mr-10 flex items-center justify-between">
+          {pathname === '/' ? (
             <li className="bg-yellow-300 hover:bg-yellow-400 rounded-md p-1 mr-3">
-              <Link href="register">
+              <Link href="/pages/register">
                 <p className="text-black text-sm">Registrar</p>
               </Link>
             </li>
+          ) : (
+            <li className={`${pathname === '/pages/register' ? 'hidden' : ''}`}>
+              <Link href="/pages/register">
+                <p className="text-white hover:text-slate-200 text-sm">
+                  Registrar
+                </p>
+              </Link>
+            </li>
           )}
-          <li>
-            <Link href="login">
+
+          <li className={`${pathname === '/pages/login' ? 'hidden' : ''}`}>
+            <Link href="/pages/login">
               <p className="text-white hover:text-slate-200 text-sm">Login</p>
             </Link>
           </li>
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,11 @@
 
 import Link from 'next/link'
 import Header from './components/header/header'
-import { useState } from 'react'
 
 export default function Home() {
-  const [showNavigation, setShowNavigation] = useState(true)
-
   return (
     <div className="flex flex-col min-h-screen">
-      <Header showNavigation={showNavigation} />
+      <Header />
 
       <main className="flex-grow">
         <Link href={'/'}>inicio</Link>
