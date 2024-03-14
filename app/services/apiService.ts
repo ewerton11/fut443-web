911e6f4bfd243f523809bfab43d4ecdb7f2e0498
ewerton reis <ewertonreisc01@gmail.com>
2024-03-13 21:19:26 -0300

    creating axios connection with api and typing to create user

---
+++ app/services/apiService.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosResponse } from 'axios'
+import { IUserData } from '../types/userData'
+
+const baseURL = 'http://localhost:5277/api'
+
+const apiService = axios.create({
+  baseURL,
+})
+
+export const createUser = async (userData: IUserData): Promise<IUserData> => {
+  try {
+    const response: AxiosResponse<IUserData> = await apiService.post(
+      '/user/create',
+      userData
+    )
+    return response.data
+  } catch (error: any) {
+    throw new Error('An error occurred while creating the user:', error)
+  }
+}
