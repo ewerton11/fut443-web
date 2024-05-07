05a372e972b4ea3edfac8b74b253aa77c249c4cd
ewerton reis <ewertonreisc01@gmail.com>
2024-05-06 21:21:55 -0300

    improved styling and responsiveness

--- app/pages/select-team/page.tsx
+++ app/pages/select-team/page.tsx
@@ -14,62 +14,69 @@ import DownArrow from '@/public/down-arrow'
 import { useEffect, useState } from 'react'
 
 export default function SelectTeam() {
-  const [players, setPlayers] = useState<PlayersData[]>([])
   const [championshipInProgress, setChampionshipInProgress] = useState<
     ChampionshipInProgessData[]
   >([])
+
+  const [players, setPlayers] = useState<PlayersData[]>([])
   const [idChampionship, setIdChampionship] = useState<string>()
   const [positionOrder, setPositionOrder] = useState<null | string>(null)
-  const [showListChampionship, setShowListChampionship] =
+
+  const [showChampionshipList, setShowChampionshipList] =
     useState<boolean>(false)
   const [isArrowRotated, setIsArrowRotated] = useState(false)
   const [teamTemporary, setTeamTemporary] = useState<PlayersData[]>([])
   const [selectedPlayers, setSelectedPlayers] = useState<string[]>([])
 
-  /*
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchChampionshipData = async () => {
       try {
         const championshipData = await getAllChampionshipInProgress()
         setChampionshipInProgress(championshipData)
-
-        
-        const params = idChampionship ?? championshipInProgress?.[0]?.id
-        const allPlayersByChampionship = await getAllPlayersByChampionshipData(
-          `${params}?position=${positionOrder}`
-        )
-        setPlayers(allPlayersByChampionship)
       } catch (error) {
-        console.error('deu erro aqui hein crllll', error)
+        console.error(
+          'Ocorreu um erro ao buscar os dados do campeonato:',
+          error
+        )
       }
     }
 
-    fetchData()
-  }, [championshipInProgress, idChampionship, positionOrder])
-*/
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const championshipData = await getAllChampionshipInProgress()
-        setChampionshipInProgress(championshipData)
-        console.log('Data', championshipData)
-      } catch (error) {}
-    }
-
-    fetchData()
+    fetchChampionshipData()
   }, [])
 
-  {
-    /*
-  const handleListChampionship = () => {
-    setShowListChampionship(!showListChampionship)
+  useEffect(() => {
+    const fetchPlayersByChampionshipData = async () => {
+      try {
+        if (!idChampionship && championshipInProgress.length > 0) {
+          setIdChampionship(championshipInProgress[0].id)
+          return
+        }
+
+        if (idChampionship) {
+          const allPlayersByChampionshipData =
+            await getAllPlayersByChampionshipData(
+              `${idChampionship}?position=${positionOrder}`
+            )
+          setPlayers(allPlayersByChampionshipData)
+        }
+      } catch (error) {
+        console.error(
+          'Ocorreu um erro ao buscar os dados dos jogadores:',
+          error
+        )
+      }
+    }
+
+    fetchPlayersByChampionshipData()
+  }, [championshipInProgress, idChampionship, positionOrder])
+
+  const toggleChampionshipList = () => {
+    setShowChampionshipList(!showChampionshipList)
     setIsArrowRotated(!isArrowRotated)
   }
 
-  const handlePlayerClick = async (playerId: string) => {
+  const handleAddPlayerToTeam = async (playerId: string) => {
     try {
-      // Não deve ter id iguais
       if (!selectedPlayers.includes(playerId)) {
         setSelectedPlayers((prevSelectedPlayers) => [
           ...prevSelectedPlayers,
@@ -77,7 +84,7 @@ export default function SelectTeam() {
         ])
 
         const teamTemporaryData = await addPlayerToTeamData(
-          '002D4417-E422-4717-B7F2-64FF0AF55605',
+          `${idChampionship}`,
           [...selectedPlayers, playerId]
         )
 
@@ -85,15 +92,32 @@ export default function SelectTeam() {
         setSelectedPlayers(newSelectedPlayers)
         setTeamTemporary(teamTemporaryData)
       } else {
-        console.log('Errorrrrr no eusi')
+        console.error('Erro: O jogador já está selecionado.')
       }
     } catch (error) {
-      console.error('Erro ao adicionar jogador:')
+      console.error('Erro ao adicionar jogador:', error)
 
       const updatedSelectedPlayers = selectedPlayers.filter(
         (player) => player !== playerId
       )
       setSelectedPlayers(updatedSelectedPlayers)
+    }
+  }
+
+  const handleRemovePlayerFromTeam = async (playerId: string) => {
+    try {
+      const updatedSelectedPlayers = selectedPlayers.filter(
+        (player) => player !== playerId
+      )
+
+      const updatedTeamTemporary = teamTemporary.filter(
+        (player) => player.id !== playerId
+      )
+
+      setSelectedPlayers(updatedSelectedPlayers)
+      setTeamTemporary(updatedTeamTemporary)
+    } catch (error) {
+      console.error('Erro ao remover jogador:', error)
     }
   }
 
@@ -109,47 +133,44 @@ export default function SelectTeam() {
   const goalkeeper = teamTemporary.filter(
     (player) => player.position === 'Goalkeeper'
   )
-*/
-  }
 
   return (
     <div className="h-full flex flex-col relative">
-      {/*
       <Header />
       <main className="w-full flex-1 flex flex-col">
-        <div className="w-full h-28 flex justify-around items-center">
-          <div className="relative w-1/3 flex justify-center items-center">
-            <p className="sm:text-sm md:text-base text-gray-600 mr-2">
+        <div className="w-full h-28 flex justify-between lg:justify-around items-center">
+          <div className="relative w-auto sm:w-1/3 flex justify-center items-center">
+            <p className="text-sm md:text-base 2xl:text-lg text-gray-600 mx-2 sm:mr-2">
               Campeonato
             </p>
             <button
               className="flex items-center bg-primary-blue p-1 rounded-md"
-              onClick={handleListChampionship}
+              onClick={toggleChampionshipList}
             >
-              <p className="sm:text-sm md:text-base text-white pl-2">
+              <p className="text-sm md:text-base 2xl:text-lg text-white pl-2">
                 {championshipInProgress?.[0]?.name}
               </p>
               <div
                 className={`transform transition-transform duration-75 ${
-                  showListChampionship ? 'rotate-180' : 'rotate-0'
+                  showChampionshipList ? 'rotate-180' : 'rotate-0'
                 }`}
               >
                 <DownArrow />
               </div>
             </button>
-            {showListChampionship && (
+            {showChampionshipList && (
               <div className="absolute top-full z-10 mt-1 w-40 bg-blue-200 rounded-md shadow-lg">
-                <ul className="py-1 sm:text-sm md:text-base text-left">
+                <ul className="py-1 text-sm md:text-base text-left">
                   {championshipInProgress.map((championship) => (
                     <li
                       key={championship.id}
                       className="border-b border-gray-200"
                     >
                       <button
-                        className="block w-full px-4 py-2 hover:bg-gray-200"
+                        className="2xl:text-lg block w-full px-4 py-2 hover:bg-gray-200"
                         onClick={(id) => {
                           setIdChampionship(championship.id)
-                          setShowListChampionship(false)
+                          setShowChampionshipList(false)
                         }}
                       >
                         {championship.name}
@@ -161,37 +182,37 @@ export default function SelectTeam() {
             )}
           </div>
 
-          <div className="w-1/3 flex justify-center items-center font-bold sm:text-base md:text-lg">
-            <div className="sm:w-8 md:w-10 sm:h-8 md:h-10 bg-gray-300 flex justify-center items-center mr-2">
+          <div className="w-1/3 flex justify-center items-center font-bold text-sm sm:text-base md:text-lg 2xl:text-xl">
+            <div className="w-8 md:w-10 h-8 md:h-10 2xl:w-12 2xl:h-12 bg-gray-300 flex justify-center items-center mr-2">
               <p>05</p>
             </div>
-            <div className="sm:w-8 md:w-10 sm:h-8 md:h-10 bg-gray-300 flex justify-center items-center ">
+            <div className="w-8 md:w-10 h-8 md:h-10 2xl:w-12 2xl:h-12 bg-gray-300 flex justify-center items-center ">
               <p>35</p>
             </div>
-            <div className="sm:w-8 md:w-10 sm:h-8 md:h-10 bg-gray-300 flex justify-center items-center ml-2">
+            <div className="w-8 md:w-10 h-8 md:h-10 2xl:w-12 2xl:h-12 bg-gray-300 flex justify-center items-center ml-2">
               <p>01</p>
             </div>
           </div>
 
-          <div className="w-1/3 flex justify-center items-center">
-            <h1 className="sm:text-base md:text-xl">SUA ESCALAÇÃO</h1>
+          <div className="hidden w-1/3 lg:flex justify-center items-center">
+            <h1 className="sm:text-base md:text-xl 2xl:text-2xl">
+              SUA ESCALAÇÃO
+            </h1>
           </div>
         </div>
 
-        <div className="min-h-screen md:flex md:flex-row-reverse">
-          <div className="w-full md:w-1/2 max-md:mb-4 flex items-center">
+        <div className="h-auto max-2xl:min-h-[90vh] 2xl:h-[75vh] lg:flex lg:flex-row-reverse justify-center">
+          <div className="w-full lg:w-auto h-[60vh] sm:h-[80vh] xl:h-screen 2xl:h-[75vh] flex justify-center items-center 2xl:ml-10">
             <FootballField
               attackers={attackers}
               midfielders={midfielders}
               defenders={defenders}
               goalkeeper={goalkeeper}
+              onPlayerRemove={handleRemovePlayerFromTeam}
             />
           </div>
-          <div className="w-full md:w-1/2 md:flex justify-center max-md:mb-4">
-            <div
-              className="w-full md:w-4/5 flex flex-col"
-              style={{ height: '90vh' }}
-            >
+          <div className="w-full lg:max-w-1/2 2xl:w-[30vw] lg:flex justify-center mb-4 2xl:mr-10">
+            <div className="w-full xl:w-4/5 h-[80vh] xl:h-screen 2xl:w-full 2xl:h-full flex flex-col">
               <ButtonGroup
                 onPositionSelect={(order: string | null) =>
                   setPositionOrder(order)
@@ -210,7 +231,8 @@ export default function SelectTeam() {
                     defenders={defenders}
                     goalkeeper={goalkeeper}
                     teamTemporary={teamTemporary}
-                    onClick={handlePlayerClick}
+                    onPlayerAdd={handleAddPlayerToTeam}
+                    onPlayerRemove={handleRemovePlayerFromTeam}
                   />
                 ))}
               </div>
@@ -219,7 +241,6 @@ export default function SelectTeam() {
         </div>
       </main>
       <Footer />
-              */}
     </div>
   )
 }
