602a5df3ae473b1f8b858215f9cca3ad0c51d7ea
ewerton reis <ewertonreisc01@gmail.com>
2024-05-06 21:19:00 -0300

    adding responsiveness to buttons

--- app/components/buttons/buttonGroup.tsx
+++ app/components/buttons/buttonGroup.tsx
@@ -8,20 +8,23 @@ const buttons = [
   { label: 'Todos', value: null },
   { label: 'ATA', value: 'Attacker' },
   { label: 'MEI', value: 'Midfielder' },
-  { label: 'ZAG', value: 'Defender' },
+  { label: 'DEF', value: 'Defender' },
   { label: 'GOL', value: 'Goalkeeper' },
 ]
 
 const ButtonGroup = ({ onPositionSelect }: ButtonOrderProps) => {
   return (
-    <div className="w-full h-16 flex justify-around">
+    <div className="w-full h-12 flex justify-around lg:justify-between">
       {buttons.map((button, index) => (
-        <div key={index} className="w-24 md:w-20 flex items-center">
+        <div
+          key={index}
+          className="w-16 sm:w-24 md:w-32 lg:w-20 flex items-center"
+        >
           <button
-            className="w-full max-sm:py-1.5 rounded-md bg-primary-blue flex justify-center items-center"
+            className="w-full py-1.5 lg:py-0 md:h-8 rounded-md bg-primary-blue flex justify-center items-center"
             onClick={() => onPositionSelect(button.value)}
           >
-            <p className="text-lg sm:text-sm md:text-base text-white">
+            <p className="text-sm sm:text-base lg:text-sm text-white">
               {button.label}
             </p>
           </button>
